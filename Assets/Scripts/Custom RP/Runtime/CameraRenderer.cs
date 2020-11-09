e0c3de7de7a997220f158cac30a0275705abe188
jbalestrierismall <julien.balestrieri@small-studio.io>
2020-11-09 14:52:03 +0100

    Initial import

---
+++ Assets/Scripts/Custom RP/Runtime/CameraRenderer.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public partial class CameraRenderer
+{
+    static ShaderTagId unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit");
+    static ShaderTagId litShaderTagId = new ShaderTagId("CustomLit");
+
+	ScriptableRenderContext context;
+
+	Camera camera;
+
+	const string bufferName = "Custom Render Camera";
+	CommandBuffer buffer = new CommandBuffer { name = bufferName };
+
+	CullingResults cullingResults;
+
+	Lighting lighting = new Lighting();
+
+	public void Render(ScriptableRenderContext context, Camera camera, bool useDynamicBatching, bool useGPUInstancing)
+    {
+		this.context = context;
+		this.camera = camera;
+
+        // Do it before culling because it can add geometry to the scene
+        PrepareBuffer();
+        PrepareForSceneWindow();
+
+		if (!Cull())
+        {
+			return;
+		}
+
+		BeginSample();
+
+		lighting.Setup(context, cullingResults);
+        DrawVisibleGeometry(useDynamicBatching, useGPUInstancing);
+	    DrawUnsupportedShaders();
+	    DrawGizmos();
+
+		EndSample();
+	}
+
+	void DrawVisibleGeometry(bool useDynamicBatching, bool useGPUInstancing)
+    {
+        // Draw opaque
+        SortingSettings sortingSettings = new SortingSettings(this.camera) {
+            criteria = SortingCriteria.CommonOpaque
+        };
+	
+        DrawingSettings drawingSettings = new DrawingSettings(unlitShaderTagId, sortingSettings) {
+			enableDynamicBatching = useDynamicBatching,
+			enableInstancing = useGPUInstancing
+		};
+		drawingSettings.SetShaderPassName(1, litShaderTagId);
+	
+		FilteringSettings filteringSettings = new FilteringSettings(RenderQueueRange.opaque);
+		context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
+
+        // Draw skybox
+		context.DrawSkybox(camera);
+
+        // Draw transparent
+        sortingSettings.criteria = SortingCriteria.CommonTransparent;
+		drawingSettings.sortingSettings = sortingSettings;
+		filteringSettings.renderQueueRange = RenderQueueRange.transparent;
+		context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
+	}
+
+	void BeginSample()
+    {
+		context.SetupCameraProperties(camera);
+        bool shouldClearDepth = camera.clearFlags <= CameraClearFlags.Depth;
+        bool shouldClearColor = camera.clearFlags == CameraClearFlags.Color;
+        Color clearColor = shouldClearColor ? camera.backgroundColor.linear : Color.clear;
+		buffer.ClearRenderTarget(shouldClearDepth, shouldClearColor, clearColor);
+		buffer.BeginSample(SampleName);
+		ExecuteBuffer();
+	}
+
+	void EndSample()
+    {
+		buffer.EndSample(SampleName);
+		ExecuteBuffer();
+		context.Submit();
+	}
+
+    bool Cull()
+    {
+		if (camera.TryGetCullingParameters(out ScriptableCullingParameters parameters))
+        {
+			cullingResults = context.Cull(ref parameters);
+			return true;
+		}
+		return false;
+	}
+
+	void ExecuteBuffer()
+    {
+		context.ExecuteCommandBuffer(buffer);
+		buffer.Clear();
+	}
+}
---
+++ Assets/Scripts/Custom RP/Runtime/Lighting.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using Unity.Collections;
+
+public class Lighting
+{
+	// Shader ids
+    static int dirLightColorsId = Shader.PropertyToID("_DirectionalLightColors");
+    static int dirLightDirectionsId = Shader.PropertyToID("_DirectionalLightDirections");
+    static int dirLightCountId = Shader.PropertyToID("_DirectionalLightCount");
+
+	// Directional lights data
+	const int maxDirLightCount = 4;
+	static Vector4[] dirLightColors = new Vector4[maxDirLightCount];
+	static Vector4[] dirLightDirections = new Vector4[maxDirLightCount];
+
+	// Buffer for profilling purpose
+	const string bufferName = "Lighting";
+	CommandBuffer buffer = new CommandBuffer {
+		name = bufferName
+	};
+
+	// Culling data
+	CullingResults cullingResults;
+	
+	public void Setup(ScriptableRenderContext context, CullingResults cullingResults)
+    {
+		this.cullingResults = cullingResults;
+
+		buffer.BeginSample(bufferName);
+		SetupLights();
+		buffer.EndSample(bufferName);
+		context.ExecuteCommandBuffer(buffer);
+		buffer.Clear();
+	}
+
+	void SetupLights()
+	{
+		int dirLightCount = 0;
+		NativeArray<VisibleLight> visibleLights = cullingResults.visibleLights;
+		for (int i = 0; i < visibleLights.Length; i++)
+		{
+			VisibleLight visibleLight = visibleLights[i];
+			if (visibleLight.lightType == LightType.Directional)
+			{
+				SetupDirectionalLight(dirLightCount, ref visibleLight);
+				dirLightCount++;
+				if (dirLightCount >= maxDirLightCount)
+				{
+					break;
+				}
+			}
+		}
+
+		buffer.SetGlobalInt(dirLightCountId, visibleLights.Length);
+		buffer.SetGlobalVectorArray(dirLightColorsId, dirLightColors);
+		buffer.SetGlobalVectorArray(dirLightDirectionsId, dirLightDirections);
+	}
+	
+	void SetupDirectionalLight(int index, ref VisibleLight visibleLight)
+	{
+		dirLightColors[index] = visibleLight.finalColor; // color including intensity
+		dirLightDirections[index] = -visibleLight.localToWorldMatrix.GetColumn(2); // forward vector
+    }
+}
