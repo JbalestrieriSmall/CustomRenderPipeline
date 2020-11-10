40cae7d3f9259072eb786a1e98fa4f04776158d2
jbalestrierismall <julien.balestrieri@small-studio.io>
2020-11-10 17:07:11 +0100

    mesh ball support for GI + lightmaps

--- Assets/Scripts/Custom RP/Examples/MeshBall.cs
+++ Assets/Scripts/Custom RP/Examples/MeshBall.cs
@@ -1,46 +1,56 @@
 ﻿using UnityEngine;
+using UnityEngine.Rendering;
 
 public class MeshBall : MonoBehaviour
 {
-	static int baseColorId = Shader.PropertyToID("_BaseColor");
-	static int metallicId = Shader.PropertyToID("_Metallic");
-	static int smoothnessId = Shader.PropertyToID("_Smoothness");
-	
-	Matrix4x4[] matrices = new Matrix4x4[1023];
-	Vector4[] baseColors = new Vector4[1023];
-	float[] metallic = new float[1023];
-	float[] smoothness = new float[1023];
+    static int baseColorId = Shader.PropertyToID("_BaseColor");
+    static int metallicId = Shader.PropertyToID("_Metallic");
+    static int smoothnessId = Shader.PropertyToID("_Smoothness");
 
-	MaterialPropertyBlock block = null;
+    Matrix4x4[] matrices = new Matrix4x4[1023];
+    Vector4[] baseColors = new Vector4[1023];
+    float[] metallic = new float[1023];
+    float[] smoothness = new float[1023];
 
-	[SerializeField]
-	Mesh mesh = default;
+    MaterialPropertyBlock block = null;
 
-	[SerializeField]
-	Material material = default;
+    [SerializeField]
+    Mesh mesh = default;
+
+    [SerializeField]
+    Material material = default;
 
     void Awake()
     {
-		for (int i = 0; i < matrices.Length; i++)
+        for (int i = 0; i < matrices.Length; i++)
         {
-			Quaternion rotation = Quaternion.Euler(Random.value * 360f, Random.value * 360f, Random.value * 360f);
-			Vector3 scale = Vector3.one * Random.Range(0.5f, 1.5f);
-			matrices[i] = Matrix4x4.TRS(Random.insideUnitSphere * 10f, rotation, scale);
-			baseColors[i] = new Vector4(Random.value, Random.value, Random.value, Random.Range(0.5f, 1f));
-			metallic[i] = Random.value < 0.25f ? 1f : 0f;
-			smoothness[i] = Random.Range(0.05f, 0.95f);
-		}
-	}
+            Quaternion rotation = Quaternion.Euler(Random.value * 360f, Random.value * 360f, Random.value * 360f);
+            Vector3 scale = Vector3.one * Random.Range(0.5f, 1.5f);
+            matrices[i] = Matrix4x4.TRS(Random.insideUnitSphere * 10f, rotation, scale);
+            baseColors[i] = new Vector4(Random.value, Random.value, Random.value, Random.Range(0.5f, 1f));
+            metallic[i] = Random.value < 0.25f ? 1f : 0f;
+            smoothness[i] = Random.Range(0.05f, 0.95f);
+        }
+    }
 
     void Update()
     {
-		if (block == null)
+        if (block == null)
         {
-			block = new MaterialPropertyBlock();
-			block.SetVectorArray(baseColorId, baseColors);
-			block.SetFloatArray(metallicId, metallic);
-			block.SetFloatArray(smoothnessId, smoothness);
-		}
-		Graphics.DrawMeshInstanced(mesh, 0, material, matrices, 1023, block);
-	}
+            block = new MaterialPropertyBlock();
+            block.SetVectorArray(baseColorId, baseColors);
+            block.SetFloatArray(metallicId, metallic);
+            block.SetFloatArray(smoothnessId, smoothness);
+            Vector3[] positions = new Vector3[1023];
+            for (int i = 0; i < matrices.Length; i++)
+            {
+                positions[i] = matrices[i].GetColumn(3);
+            }
+            SphericalHarmonicsL2[] lightProbes = new SphericalHarmonicsL2[1023];
+            LightProbes.CalculateInterpolatedLightAndOcclusionProbes(positions, lightProbes, null);
+			block.CopySHCoefficientArraysFrom(lightProbes);
+        }
+        Graphics.DrawMeshInstanced(mesh, 0, material, matrices, 1023, block,
+            ShadowCastingMode.On, true, 0, null, LightProbeUsage.CustomProvided);
+    }
 }
--- Assets/Scripts/Custom RP/Shaders/Lit.shader
+++ Assets/Scripts/Custom RP/Shaders/Lit.shader
@@ -27,6 +27,10 @@
 		// Shadow
 		[KeywordEnum(On, Clip, Dither, Off)] _Shadows("Shadows", Float) = 0
 		[Toggle(_RECEIVE_SHADOWS)] _ReceiveShadows("Receive Shadows", Float) = 1
+
+		// Lightmaps, allow to bake transparency
+		[HideInInspector] _MainTex("Texture for Lightmap", 2D) = "white" {}
+		[HideInInspector] _Color("Color for Lightmap", Color) = (0.5, 0.5, 0.5, 1.0)
     }
 
     SubShader
